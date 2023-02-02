2029b02f8991c0ee1553299e47173cc2a88cee78
jdhutchison <github@hutch.sh>
2023-02-03 10:59:32 +1100

    Fixed up comment in test

--- src/test/java/sh/hutch/innablr/challenge/ChallengeApplicationTests.java
+++ src/test/java/sh/hutch/innablr/challenge/ChallengeApplicationTests.java
@@ -3,6 +3,9 @@ package sh.hutch.innablr.challenge;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Simple test to make sure the configuration is error-free.
+ */
 @SpringBootTest
 class ChallengeApplicationTests {
 
